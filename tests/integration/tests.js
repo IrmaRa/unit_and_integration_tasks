184b1fb4beecd0023f9c658c49dbe33f89d06718
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-09 14:31:26 +0100

    Test if works with negative and large numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -44,5 +44,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('12')
   });
 
+  it('should work when negative, large numbers or decimals are pressed', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-600')
+  });
 
 });
