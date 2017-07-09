eb42f393e523fd2c4c980c2d80f86d4822d67597
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-09 14:48:39 +0100

    Correct the code to give a message when trying to divide by zero

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -70,5 +70,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
   });
 
+  it('should give error message if try to divide by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('division by zero is undefined')
+  });
 
 });
